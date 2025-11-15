b66620212ba48cd4c2be4d5fa51e1eacb5234928
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-11-15 23:36:12 +0000

    Make the key pressing and changing words work

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const textstrings = ["hello!", "how are you?"]
+
+var i = 0
+var currentstring = 0
+
 document.addEventListener("keydown", event => {
-    console.log(event.key)
-});
+    if ((currentstring+1 > textstrings.length)==false) {
+        var letter = textstrings[currentstring].substring(i, i+1);
+        if (letter == '') {
+                currentstring += 1;
+                i = 0;
+                letter = textstrings[currentstring].substring(i, i+1);
+        };
+        if (event.key == letter) {
+            console.log("correct");
+            i += 1;
+        }
+        else {
+            console.log("false, correct was " + letter);
+        };
+        console.log(i);
+}});
