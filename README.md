c8f4086db4e2f6aed0b30f105f1962bba6f0f3d4
yanellaft <yanellaft@gmail.com>
2025-11-15 08:28:25 -0500

    tryout

--- README.md
+++
@@ -1,3 +0,0 @@
-a game we are making for parthenon!
-
-made by yanella, sara, and safia
--- sketch.js
+++ sketch.js
@@ -1,30 +1,7 @@
-let screen = 0;
-
-let startButton;
-
 function setup() {
   createCanvas(400, 400);
-
-  startButton = new Sprite(-200, -200, 100, 50);
-  startButton.text = "Enter War"; // fix this title !!!
-  startButton.color = "red";
-  startButton.static = true;
 }
 
 function draw() {
   background(220);
-
-  //SCREEN 0 --> Welcome Screen
-  if (screen == 0) {
-    textSize(32);
-    textAlign(CENTER);
-    text("The War of Gods", width / 2, height / 2);
-    textSize(16);
-
-    startButton.pos = {x: 200, y: 200};
-    if (startButton.mouse.pressed()) {
-      screen = 1;
-      print("screen1");
-    }
-  }
 }
