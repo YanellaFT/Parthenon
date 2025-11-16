ca76ed5a4bcb52b3de05a3e148544b60e7370671
yanellaft <yanellaft@gmail.com>
2025-11-16 02:03:36 -0500

    testing purposes

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let timeLeft = countdownTime;
 
 let currentScreen = 0; // 0 = title, 1 = intro, 2 = challeng 1 intro, 3 = challenge 1, 4 = challenge 2 intro, 5 = challenge 2
 
-const textstrings = ["Ahhh! Get away from me! I do NOT want your cookies! Let me goooo!!!", "I am so precise. Being precise is awesome, right? I am more precise than you, hahahaha… No. I cannot turn evil too. I am just doing this to stay alive. Wait, am I winning against THE Artemis???"]
-//const textstrings = ["j", "k"];
+//const textstrings = ["Ahhh! Get away from me! I do NOT want your cookies! Let me goooo!!!", "I am so precise. Being precise is awesome, right? I am more precise than you, hahahaha… No. I cannot turn evil too. I am just doing this to stay alive. Wait, am I winning against THE Artemis???"]
+const textstrings = ["j", "k"]; //for testing purposes
 
 let i = 0;
 let currentstring = 0;
