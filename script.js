f20bb5e03bc9723a8f3fb15988ca989da1f1c26e
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-11-16 00:16:02 +0000

    Add text prompt

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var i = 0
 var currentstring = 0
 
 document.addEventListener("keydown", event => {
+    document.getElementById("prompt").textContent += textstrings[currentstring];
     if ((currentstring+1 > textstrings.length)==false) {
         var letter = textstrings[currentstring].substring(i, i+1);
         if (letter == '') {
