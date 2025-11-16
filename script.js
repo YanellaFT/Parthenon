1df19fdc7459095ddacb4508b3cc562141802f01
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-11-16 01:19:22 +0000

    Add a button to start

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const textstrings = ["hello!", "how are you?"]
 var i = 0
 var currentstring = 0
 
+function onStartPressed() {
+    console.log("started");
+    document.getElementById("prompt").textContent = textstrings[currentstring];
+}
+
 document.addEventListener("keydown", event => {
-    document.getElementById("prompt").textContent += textstrings[currentstring];
     if ((currentstring+1 > textstrings.length)==false) {
         var letter = textstrings[currentstring].substring(i, i+1);
         if (letter == '') {
                 currentstring += 1;
                 i = 0;
+                document.getElementById("prompt").textContent = textstrings[currentstring];
                 letter = textstrings[currentstring].substring(i, i+1);
         };
         if (event.key == letter) {
